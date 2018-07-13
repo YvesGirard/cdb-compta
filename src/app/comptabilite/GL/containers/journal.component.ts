9045db685be59f216dd270e4001f5d7a959a2368
GIRARD <yves.girard@capgemini.com>
2018-07-13 08:28:49 +0200

    commit 130

---
+++ src/app/comptabilite/GL/containers/journal.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+//import * as CollectionActions from '../actions/collection.actions';
+//import { Book } from '../models/book';
+//import * as fromBooks from '../reducers';
+
+@Component({
+    selector: 'gl-journal-page',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    template: `
+    <gl-journal-list [journals]="journals$ | async"></gl-journal-list>
+  `,
+    /**
+     * Container components are permitted to have just enough styles
+     * to bring the view together. If the number of styles grow,
+     * consider breaking them out into presentational
+     * components.
+     */
+    styles: [
+        `
+  `,
+    ],
+})
+export class SetupPageComponent implements OnInit {
+
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
